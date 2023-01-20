73f5d5db94e4923e9b35a3edc26fd7f3de3c57d0
Louie Lin <607430084louie@alum.ccu.edu.tw>
2023-01-20 16:08:12 +0900

    MOD: Light process of game mode DEL: Unuse texture ADD: Start, Quit, texture for title

--- light.cpp
+++ light.cpp
@@ -52,31 +52,32 @@ static	BOOL	g_FogEnable = true;
 static int		g_nowTime = SUNRISE;
 static float	g_time = -1;
 
-XMFLOAT3 RiseToNoon = XMFLOAT3((LIGHT_NOON_POS_X - LIGHT_SUNRISE_POS_X) / SUN_MOVING_TIME,
+XMFLOAT3 RiseToNoon = XMFLOAT3(
+	(LIGHT_NOON_POS_X - LIGHT_SUNRISE_POS_X) / SUN_MOVING_TIME,
 	(LIGHT_NOON_POS_Y - LIGHT_SUNRISE_POS_Y) / SUN_MOVING_TIME,
 	(LIGHT_NOON_POS_Z - LIGHT_SUNRISE_POS_Z) / SUN_MOVING_TIME
 );
-XMFLOAT3 NoonToSet = XMFLOAT3((LIGHT_SUNSET_POS_X - LIGHT_NOON_POS_X) / SUN_MOVING_TIME,
+XMFLOAT3 NoonToSet = XMFLOAT3(
+	(LIGHT_SUNSET_POS_X - LIGHT_NOON_POS_X) / SUN_MOVING_TIME,
 	(LIGHT_SUNSET_POS_Y - LIGHT_NOON_POS_Y) / SUN_MOVING_TIME,
 	(LIGHT_SUNSET_POS_Z - LIGHT_NOON_POS_Z) / SUN_MOVING_TIME
 );
-XMFLOAT3 SetToMidnight = XMFLOAT3((LIGHT_MIDNINGT_POS_X - LIGHT_SUNSET_POS_X) / SUN_MOVING_TIME,
+XMFLOAT3 SetToMidnight = XMFLOAT3(
+	(LIGHT_MIDNINGT_POS_X - LIGHT_SUNSET_POS_X) / SUN_MOVING_TIME,
 	(LIGHT_MIDNINGT_POS_Y - LIGHT_SUNSET_POS_Y) / SUN_MOVING_TIME,
 	(LIGHT_MIDNINGT_POS_Z - LIGHT_SUNSET_POS_Z) / SUN_MOVING_TIME
 );
-XMFLOAT3 MidnightToRise = XMFLOAT3((LIGHT_SUNRISE_POS_X - LIGHT_MIDNINGT_POS_X) / SUN_MOVING_TIME,
+XMFLOAT3 MidnightToRise = XMFLOAT3(
+	(LIGHT_SUNRISE_POS_X - LIGHT_MIDNINGT_POS_X) / SUN_MOVING_TIME,
 	(LIGHT_SUNRISE_POS_Y - LIGHT_MIDNINGT_POS_Y) / SUN_MOVING_TIME,
 	(LIGHT_SUNRISE_POS_Z - LIGHT_MIDNINGT_POS_Z) / SUN_MOVING_TIME
 );
-
 
 //=============================================================================
 // 初期化処理
 //=============================================================================
 void InitLight(void)
 {
-	static float	g_time = -1;
-
 	//ライト初期化
 	for (int i = 0; i < LIGHT_MAX; i++)
 	{
@@ -90,23 +91,47 @@ void InitLight(void)
 		SetLight(i, &g_Light[i]);
 	}
 
-	// 並行光源の設定（世界を照らす光）
-	g_Light[0].Position = XMFLOAT3(LIGHT_SUNRISE_POS_X, LIGHT_SUNRISE_POS_Y, LIGHT_SUNRISE_POS_Z);
-	g_Light[0].Direction = XMFLOAT3(0 - g_Light[0].Position.x, 0 - g_Light[0].Position.y, 0 - g_Light[0].Position.z);		// 光の向き
-	g_Light[0].Diffuse = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);	// 光の色
-	g_Light[0].Type = LIGHT_TYPE_DIRECTIONAL;					// 並行光源
-	g_Light[0].Enable = true;									// このライトをON
-	SetLight(0, &g_Light[0]);									// これで設定している
+	switch (GetMode())
+	{
+	case MODE_TITLE:
+		g_nowTime = SUNRISE;
+		
+		// 並行光源の設定（世界を照らす光）
+		g_Light[0].Position = XMFLOAT3(LIGHT_SUNRISE_POS_X, LIGHT_SUNRISE_POS_Y, LIGHT_SUNRISE_POS_Z);
+		g_Light[0].Direction = XMFLOAT3(0 - g_Light[0].Position.x, 0 - g_Light[0].Position.y, 0 - g_Light[0].Position.z);		// 光の向き
+		g_Light[0].Diffuse = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);	// 光の色
+		g_Light[0].Type = LIGHT_TYPE_DIRECTIONAL;					// 並行光源
+		g_Light[0].Enable = true;									// このライトをON
+		SetLight(0, &g_Light[0]);									// これで設定している
 
+		// フォグの初期化（霧の効果）
+		g_Fog.FogStart = 1000.0f;									// 視点からこの距離離れるとフォグがかかり始める
+		g_Fog.FogEnd = 10000.0f;									// ここまで離れるとフォグの色で見えなくなる
+		g_Fog.FogColor = XMFLOAT4(0.0f, 0.0f, 0.0f, 1.0f);		// フォグの色
+		SetFog(&g_Fog);
+		SetFogEnable(g_FogEnable);		// 他の場所もチェックする shadow
+		break;
+		
+	case MODE_GAME:
+		g_nowTime = NOON;
+		
+		// 並行光源の設定（世界を照らす光）
+		g_Light[0].Position = XMFLOAT3(LIGHT_NOON_POS_X, LIGHT_NOON_POS_Y, LIGHT_NOON_POS_Z);
+		g_Light[0].Direction = XMFLOAT3(0 - g_Light[0].Position.x, 0 - g_Light[0].Position.y, 0 - g_Light[0].Position.z);		// 光の向き
+		g_Light[0].Diffuse = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);	// 光の色
+		g_Light[0].Type = LIGHT_TYPE_DIRECTIONAL;					// 並行光源
+		g_Light[0].Enable = true;									// このライトをON
+		SetLight(0, &g_Light[0]);									// これで設定している
 
-
-	// フォグの初期化（霧の効果）
-	g_Fog.FogStart = 1000.0f;									// 視点からこの距離離れるとフォグがかかり始める
-	g_Fog.FogEnd   = 10000.0f;									// ここまで離れるとフォグの色で見えなくなる
-	g_Fog.FogColor = XMFLOAT4( 0.0f, 0.0f, 0.0f, 1.0f );		// フォグの色
-	SetFog(&g_Fog);
-	SetFogEnable(g_FogEnable);		// 他の場所もチェックする shadow
-
+		// フォグの初期化（霧の効果）
+		g_Fog.FogStart = 1000.0f;									// 視点からこの距離離れるとフォグがかかり始める
+		g_Fog.FogEnd = 10000.0f;									// ここまで離れるとフォグの色で見えなくなる
+		g_Fog.FogColor = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);		// フォグの色
+		SetFog(&g_Fog);
+		SetFogEnable(g_FogEnable);		// 他の場所もチェックする shadow
+	}
+	
+	g_time = -1;
 }
 
 
@@ -116,71 +141,79 @@ void InitLight(void)
 void UpdateLight(void)
 {
 	g_time++;
-	
-	switch (g_nowTime)
+	switch (GetMode())
 	{
-	case SUNRISE:
-		if (g_time < SUN_MOVING_TIME)
+	case MODE_TITLE:
+		
+		switch (g_nowTime)
 		{
-			g_Light[0].Position.x = LIGHT_SUNRISE_POS_X + g_time * RiseToNoon.x;
-			g_Light[0].Position.y = LIGHT_SUNRISE_POS_Y + g_time * RiseToNoon.y;
-			g_Light[0].Position.z = LIGHT_SUNRISE_POS_Z + g_time * RiseToNoon.z;
-			
-			g_Fog.FogColor.x += 1.0f / SUN_MOVING_TIME;
-			g_Fog.FogColor.y += 1.0f / SUN_MOVING_TIME;
-			g_Fog.FogColor.z += 1.0f / SUN_MOVING_TIME;
-		}
-		else
-		{
-			g_time = -1;
-			g_nowTime = NOON;
+		case SUNRISE:
+			if (g_time < SUN_MOVING_TIME)
+			{
+				g_Light[0].Position.x = LIGHT_SUNRISE_POS_X + g_time * RiseToNoon.x;
+				g_Light[0].Position.y = LIGHT_SUNRISE_POS_Y + g_time * RiseToNoon.y;
+				g_Light[0].Position.z = LIGHT_SUNRISE_POS_Z + g_time * RiseToNoon.z;
+
+				g_Fog.FogColor.x += 1.0f / SUN_MOVING_TIME;
+				g_Fog.FogColor.y += 1.0f / SUN_MOVING_TIME;
+				g_Fog.FogColor.z += 1.0f / SUN_MOVING_TIME;
+			}
+			else
+			{
+				g_time = -1;
+				g_nowTime = NOON;
+			}
+			break;
+
+		case NOON:
+			if (g_time < SUN_MOVING_TIME)
+			{
+				g_Light[0].Position.x = LIGHT_NOON_POS_X + g_time * NoonToSet.x;
+				g_Light[0].Position.y = LIGHT_NOON_POS_Y + g_time * NoonToSet.y;
+				g_Light[0].Position.z = LIGHT_NOON_POS_Z + g_time * NoonToSet.z;
+
+				g_Fog.FogColor.x -= 1.0f / SUN_MOVING_TIME;
+				g_Fog.FogColor.y -= 1.0f / SUN_MOVING_TIME;
+				g_Fog.FogColor.z -= 1.0f / SUN_MOVING_TIME;
+			}
+			else
+			{
+				g_time = -1;
+				g_nowTime = SUNSET;
+			}
+			break;
+
+		case SUNSET:
+			if (g_time < SUN_MOVING_TIME)
+			{
+				g_Light[0].Position.x = LIGHT_SUNSET_POS_X + g_time * SetToMidnight.x;
+				g_Light[0].Position.y = LIGHT_SUNSET_POS_Y + g_time * SetToMidnight.y;
+				g_Light[0].Position.z = LIGHT_SUNSET_POS_Z + g_time * SetToMidnight.z;
+			}
+			else
+			{
+				g_time = -1;
+				g_nowTime = MIDNIGHT;
+			}
+			break;
+
+		case MIDNIGHT:
+			if (g_time < SUN_MOVING_TIME)
+			{
+				g_Light[0].Position.x = LIGHT_MIDNINGT_POS_X + g_time * MidnightToRise.x;
+				g_Light[0].Position.y = LIGHT_MIDNINGT_POS_Y + g_time * MidnightToRise.y;
+				g_Light[0].Position.z = LIGHT_MIDNINGT_POS_Z + g_time * MidnightToRise.z;
+			}
+			else
+			{
+				g_time = -1;
+				g_nowTime = SUNRISE;
+			}
+			break;
 		}
 		break;
 
-	case NOON:
-		if (g_time < SUN_MOVING_TIME)
-		{
-			g_Light[0].Position.x = LIGHT_NOON_POS_X + g_time * NoonToSet.x;
-			g_Light[0].Position.y = LIGHT_NOON_POS_Y + g_time * NoonToSet.y;
-			g_Light[0].Position.z = LIGHT_NOON_POS_Z + g_time * NoonToSet.z;
-
-			g_Fog.FogColor.x -= 1.0f / SUN_MOVING_TIME;
-			g_Fog.FogColor.y -= 1.0f / SUN_MOVING_TIME;
-			g_Fog.FogColor.z -= 1.0f / SUN_MOVING_TIME;
-		}
-		else
-		{
-			g_time = -1;
-			g_nowTime = SUNSET;
-		}
-		break;
-
-	case SUNSET:
-		if (g_time < SUN_MOVING_TIME)
-		{
-			g_Light[0].Position.x = LIGHT_SUNSET_POS_X + g_time * SetToMidnight.x;
-			g_Light[0].Position.y = LIGHT_SUNSET_POS_Y + g_time * SetToMidnight.y;
-			g_Light[0].Position.z = LIGHT_SUNSET_POS_Z + g_time * SetToMidnight.z;
-		}
-		else
-		{
-			g_time = -1;
-			g_nowTime = MIDNIGHT;
-		}
-		break;
-
-	case MIDNIGHT:
-		if (g_time < SUN_MOVING_TIME)
-		{
-			g_Light[0].Position.x = LIGHT_MIDNINGT_POS_X + g_time * MidnightToRise.x;
-			g_Light[0].Position.y = LIGHT_MIDNINGT_POS_Y + g_time * MidnightToRise.y;
-			g_Light[0].Position.z = LIGHT_MIDNINGT_POS_Z + g_time * MidnightToRise.z;
-		}
-		else
-		{
-			g_time = -1;
-			g_nowTime = SUNRISE;
-		}
+	case MODE_GAME:
 		break;
 	}
 
